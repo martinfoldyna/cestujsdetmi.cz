36e298d0eaa92349adb1059fc97cec65c8f95228
martin.foldyna@ackee.cz <martin.foldyna@ackee.cz>
2021-04-28 14:45:15 +0200

    Rss added

--- api/rss/controllers/rss.js
+++ api/rss/controllers/rss.js
@@ -1,31 +1,42 @@
 'use strict';
-const NodeCache = require( "node-cache" );
-const myCache = new NodeCache({stdTTL: 60, checkperiod: 90});
+const helpers = require("../../../helpers/helpers");
 
 /**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
+
 module.exports = {
-  async find(ctx) {
-    const rss = await strapi.services.rss.getRssFromStorage();
+  async create(ctx) {
+    let entity;
+    const rssJSONData = await helpers.parseXml(ctx.request.body.EVENT);
 
-    console.log(rss.data)
-
-
-    if (rss.data === undefined) {
-      return { success: false, data: [], error: {status: 404, statusMessage: "Not Found"} }
+    if (Array.isArray(rssJSONData)) {
+      entity = [];
+      for (let data of rssJSONData.filter(item => item.VHODNEPRORODICESDETMI === "ANO")) {
+        const uploadedObject = {
+          nazev: data.NAMECZ,
+          misto: data.MISTOCZ,
+          popis: data.POPISCZ,
+          img: data.IMG,
+          link: data.LINKCZ,
+          dateFrom: data.DATEFROM,
+          dateTo: data.DATETO,
+        }
+        const newEntity = await strapi.services.rss.create(uploadedObject);
+        entity.push(newEntity)
+      }
+    } else {
+      entity = await strapi.services.rss.create(ctx.request.body);
     }
-
-      return { success: true, data: rss.data, error: null }
-
+    return entity;
   },
-  async findOne(ctx) {
-    const { id } = ctx.params;
+  async getPaths(ctx) {
+    const entites = await strapi
+      .query("rss").model.find()
+      .select("id");
 
-    const rss = await strapi.services.rss.fetchRssById(id);
-
-    return rss
+    return entites
   }
 };

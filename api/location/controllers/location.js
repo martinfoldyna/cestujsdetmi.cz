006ca531448523d4e0f6f549b0c3206e723b92b7
Martin Foldyna <martin.foldyna@ackee.cz>
2021-06-08 13:49:12 +0200

    performance

--- api/location/controllers/location.js
+++ api/location/controllers/location.js
@@ -6,65 +6,29 @@ const todayString = today.toISOString()
 module.exports = {
   async findAll(ctx) {
     let finalRes = { mesta: [], kraje: [], oblasti: [], kategorie: [], ads: [] };
-    const cityEntities = await strapi
+    finalRes.mesta = await strapi
       .query("mesto")
       .model.find(ctx.query)
-      .select("value key kraj_id");
-    finalRes.mesta = cityEntities.sort((a, b) => {
-      if (a.value < b.value) {
-        return -1;
-      }
-      if (a.value > b.value) {
-        return 1;
-      }
-      return 0;
-    });
+      .select("value key kraj_id").sort("value");
 
     finalRes.ads = await strapi
       .query("reklamni-banner")
-      .model.find({ datum_zobrazeni_od: { $lte: todayString }, datum_zobrazeni_do: {$gte: todayString} })
+      .model.find({ datum_zobrazeni_od: { $lte: todayString }, datum_zobrazeni_do: {$gte: todayString} }).select("script umisteni")
       .sort("-datum_zobrazeni_do");
 
-    const krajEntities = await strapi
+    finalRes.kraje = await strapi
       .query("kraj")
       .model.find(ctx.query)
-      .select("value key old_id");
+      .select("value key old_id").sort({value: "asc"});
 
-    finalRes.kraje = krajEntities.sort((a, b) => {
-      if (a.value < b.value) {
-        return -1;
-      }
-      if (a.value > b.value) {
-        return 1;
-      }
-      return 0;
-    });
-
-    const oblastEntites = await strapi
+    finalRes.oblasti = await strapi
       .query("oblast")
       .model.find(ctx.query)
-      .select("value key kraj");
+      .select("value key kraj").sort({value: "asc"});
 
-    finalRes.oblasti = oblastEntites.sort((a, b) => {
-      if (a.value < b.value) {
-        return -1;
-      }
-      if (a.value > b.value) {
-        return 1;
-      }
-      return 0;
-    });
-
-    const categoryEntities = await strapi.services.kategorie.find(ctx.query);
-    finalRes.kategorie = categoryEntities.sort((a, b) => {
-      if (a.value < b.value) {
-        return -1;
-      }
-      if (a.value > b.value) {
-        return 1;
-      }
-      return 0;
-    });
+    finalRes.kategorie = await strapi
+      .query("kategorie")
+      .model.find(ctx.query).populate({ path: "podkategorie", select: "nazev hodnota" }).sort({value: "asc"});
 
     return finalRes;
   },
